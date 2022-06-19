4b5d85e4896ef26c111337719f565af8d726011a
Nick Taylor <nick@iamdeveloper.com>
2022-06-18 22:55:09 -0400

    changed default number of videos to 6

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -53,7 +53,7 @@ function generateVideosMarkup(videos) {
  *  }
  * ]
  */
-async function getVideos(videoFeedUrl, numberOfVideos = 3) {
+async function getVideos(videoFeedUrl, numberOfVideos = 6) {
   const parser = new Parser({
     customFields: {
       item: ['media:group', 'media:thumbnail'],
